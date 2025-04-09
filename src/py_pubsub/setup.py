74ff92894fb19be79a381843801f83a6f5e03854
Carter Stone <26511753@students.lincoln.ac.uk>
2025-04-09 19:00:06 +0000

    updated package

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'find colour = py_pubsub.colour_chaser_function:main',
             'talker = py_pubsub.publisher_member_function:main',
             'listener = py_pubsub.subscriber_member_function:main',
         ],
